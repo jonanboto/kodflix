d8a8b6b9a1400bcaa2929d1a69e1c91da3466667
Jonan Boto <jonan.boto@gmail.com>
2019-01-07 16:07:08 +0000

    Added 6 movie titles in grid format, with movie image below

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import moviecover from './rumble-image.jpg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <br /><br /><br />
+        <br /><br /><br />
+        <div className="container">
+          <div className="title">ReactJs</div>
+          <div className="title">HTML5</div>
+          <div className="title">CSS3</div>
+        </div>
+        <div className="container">
+          <div className="title">Node.js</div>
+          <div className="title">MongoDB</div>
+          <div className="title">Heroku</div>
+        </div>
+        <img src={moviecover} alt='Rumble in the Jungle' />
+        <br /><br /><br />
       </div>
     );
   }
