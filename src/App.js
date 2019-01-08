a5211b5e296c5e33d542702c0448bfa535bf238d
Jonan Boto <jonan.boto@gmail.com>
2019-01-08 13:18:29 +0000

    replaced 6 movie titles with 6 images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
-import moviecover from './rumble-image.jpg';
+import ali from './images/ali.jpg';
+import elephant from './images/elephant.jpg';
+import joplin from './images/joplin.jpg';
+import lincoln from './images/lincoln.jpg'
+import retrotv from './images/retrotv.jpg'
+import stpancras from './images/stpancras.jpg'
 import './App.css';
 
 class App extends Component {
@@ -9,21 +14,32 @@ class App extends Component {
         <br /><br /><br />
         <br /><br /><br />
         <div className="container">
-          <div className="title">ReactJs</div>
-          <div className="title">HTML5</div>
-          <div className="title">CSS3</div>
+          <div className="item">
+            <img src={ali} alt='Muhammad Ali' />
+          </div>
+          <div className="item">
+            <img src={elephant} alt='Elephant facing front on' />
+          </div>
+          <div className="item">
+            <img src={joplin} alt='Janis Joplin playing guitar' />
+          </div>
         </div>
-        <br /><br /><br />
+        <br /> <br /> <br />
         <div className="container">
-          <div className="title">Node.js</div>
-          <div className="title">MongoDB</div>
-          <div className="title">Heroku</div>
+          <div className="item">
+            <img src={lincoln} alt='Abraham Lincoln set back in colour' />
+          </div>
+          <div className="item">
+            <img src={retrotv} alt='1960s style tv on stand' />
+          </div>
+          <div className="item">
+            <img src={stpancras} alt='St Pancras railway station architecture' />
+          </div>
         </div>
-        <br /><br /><br />
-        <br /><br /><br />
-        <img src={moviecover} alt='Rumble in the Jungle' />
-        <br /><br /><br />
-      </div>
+        <br /> <br /> <br />
+        <br /> <br /> <br />
+        <br /> <br /> <br />
+      </div >
     );
   }
 }
