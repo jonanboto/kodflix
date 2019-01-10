05f056cce6a61aa221ea5d6e0bb2bc33d132fdc6
Jonan Boto <jonan.boto@gmail.com>
2019-01-10 19:04:51 +0000

    Added title overlays triggered by hovering over image

--- src/App.js
+++ src/App.js
@@ -17,35 +17,50 @@ class App extends Component {
         <br />
         <br />
         <br />
-
         <div className="container">
           <div className="item">
             <img src={ali} alt='Muhammad Ali' />
+            <div className="titleoverlay">
+              <h3>Rumble in the Jungle</h3>
+            </div>
           </div>
           <div className="item">
             <img src={elephant} alt='Elephant facing front on' />
+            <div className="titleoverlay">
+              <h3>Graced with life</h3>
+            </div>
           </div>
           <div className="item">
             <img src={joplin} alt='Janis Joplin playing guitar' />
+            <div className="titleoverlay">
+              <h3>Summer of love</h3>
+            </div>
           </div>
         </div>
         <br />
         <br />
-
         <div className="container">
           <div className="item">
             <img src={lincoln} alt='Abraham Lincoln set back in colour' />
+            <div className="titleoverlay">
+              <h3>A nation scarred</h3>
+            </div>
           </div>
           <div className="item">
             <img src={retrotv} alt='1960s style tv on stand' />
+            <div className="titleoverlay">
+              <h3>A life less ordinary</h3>
+            </div>
           </div>
           <div className="item">
             <img src={stpancras} alt='St Pancras railway station architecture' />
+            <div className="titleoverlay">
+              <h3>Beauty from bricks</h3>
+            </div>
           </div>
         </div>
         <br />
         <br />
-
       </div >
     );
   }
