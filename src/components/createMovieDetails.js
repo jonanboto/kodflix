21abd1c5fc82f3a293f18728ff21075cacb68df9
Jonan Boto <jonan.boto@gmail.com>
2019-01-22 12:40:23 +0000

    showed movie title on respective details page

--- src/components/createMovieDetails.js
+++ src/components/createMovieDetails.js
@@ -1,28 +1,28 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom'
+import MovieData from './getMovieData';
 
 export default class MovieDetails extends React.Component {
   
   constructor() {
     super();
     this.state = {
-        welcomeMessage: 'Welcome to the details page, WIP!'
+        movie: {}
     };
   }
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        welcomeMessage: 'hi there'
-      });
-    }, 3000);
+    let movieId = this.props.match.params.movieId;
+    let movie = MovieData()
+      .find(movie => movie.id === movieId);
+    this.setState({ movie });
   }
 
   render() {
     return (
       <div>
-        <h1>{this.state.welcomeMessage}</h1>
+        <h1>{this.state.movie.name}</h1>
         <Link to='/'><h3>Back to homepage</h3></Link>
       </div>
     );
