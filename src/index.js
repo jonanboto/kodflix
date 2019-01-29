c0029f7b0dbad60a118aefe406aedd8b13144030
Jonan Boto <jonan.boto@gmail.com>
2019-01-29 20:08:05 +0000

    initial (failed) attempt to install heroku addin by modifying index.js

--- src/index.js
+++ src/index.js
@@ -14,3 +14,20 @@ serviceWorker.unregister();
 if(module.hot) {
     module.hot.accept();
 }
+
+// Jonan: not been able to successfully install heroku add-in to push local changes to git and herokU:
+// https://devcenter.heroku.com/articles/getting-started-with-nodejs#push-local-changes
+
+// sample proposed code to add to index.js (from heroku helpdesk site, referenced above)
+// const cool = require('cool-ascii-faces')
+// const express = require('express')
+// const path = require('path')
+// const PORT = process.env.PORT || 5000
+
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('pages/index'))
+//   .get('/cool', (req, res) => res.send(cool()))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
