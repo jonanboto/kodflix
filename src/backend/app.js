bad9a6d6e9787b2237c00f8635bf8fabde2117b8
Jonan Boto <jonan.boto@gmail.com>
2019-02-02 22:44:45 +0000

    test paps

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3000;
+const movieData = require('./movieData');
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
+app.get('/rest/movieData', (req, res) => res.send(movieData()))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
