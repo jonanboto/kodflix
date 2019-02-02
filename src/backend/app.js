0da4698dff0cdcd4dd0d09a7053e8549497344c9
Jonan Boto <jonan.boto@gmail.com>
2019-02-02 23:05:34 +0000

    updated labels for backend file

--- src/backend/app.js
+++ src/backend/app.js
@@ -3,5 +3,5 @@ const app = express()
 const port = 3000;
 const movieData = require('./movieData');
 
-app.get('/rest/movieData', (req, res) => res.send(movieData()))
+app.get('/rest/movies', (req, res) => res.send(movieData()))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
