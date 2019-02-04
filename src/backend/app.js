dfada5f5d5c0e9d6412941c7c044b233c9c0336b
Jonan Boto <jonan.boto@gmail.com>
2019-02-04 14:11:05 +0000

    installed npm-run-all. And changed backend server port to 3001 (vs 3000)

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000;
+const port = 3001;
 const movieData = require('./movieData');
 
 app.get('/rest/movies', (req, res) => res.send(movieData()))
