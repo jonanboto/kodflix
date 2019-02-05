aa465064d73cc859f6ac16f57a76b418acf18777
Jonan Boto <jonan.boto@gmail.com>
2019-02-05 11:06:38 +0000

    created build folder (npm run build) editied app.js (and installed path)

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 3001;
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3001;
 const movieData = require('./movieData');
 
-app.get('/rest/movies', (req, res) => res.send(movieData()))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const path = require('path');
+
+app.listen(port, () => console.log(`Example app listening on port ${path.join(__dirname, '../../build')}!`));
+app.get('/rest/movies', (req, res) => res.send(movieData()));
+
+app.use(express.static(path.join(__dirname, '../../build')));
+app.get('*', function (req, res) { res.sendFile(path.join(__dirname, '../../build', 'index.html')); });
